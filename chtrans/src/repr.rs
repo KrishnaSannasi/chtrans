797f5333ce1afd47c8e1e05dfb55de09a3907914
Ozaren <krishna.sd.2012@gmail.com>
2019-12-31 13:53:10 -0500

    added docs for repr

--- chtrans/src/repr.rs
+++ chtrans/src/repr.rs
@@ -1,8 +1,19 @@
+//! The specification for the high level representation of types
+//! 
+//! [struct](crate::repr::r#struct) contains the specification for structs
+//! 
+
 use crate::hlist::*;
 use crate::{Type, slots::{self, Uninit, SlotList}};
 use typenum::{*, marker_traits::PowerOfTwo, operator_aliases::Mod};
 use core::ops::{Rem, Sub};
 
+/// specifies that the fields of a type must be laid out with `C`'s
+/// field layout algorithm.
 pub enum ReprC {}
+
+/// specifies that the fields of a type must be laid out with in order
+/// with no padding bytes
+pub enum ReprPacked {}
 
 pub mod r#struct;
