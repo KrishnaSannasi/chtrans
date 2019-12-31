9312bb357e7bf18d99b30e2bbed4418b7c2d5a9c
Ozaren <krishna.sd.2012@gmail.com>
2019-12-31 13:05:13 -0500

    added docs for slots

--- chtrans/src/slots.rs
+++ chtrans/src/slots.rs
@@ -1,19 +1,50 @@
+
+//! The specification of the memory layout
+//! 
+//! Memory is a linked list of bytes or markers, bytes take up a single byte of
+//! space, and markers take up no space at all. Memory bytes do not have a notion
+//! of alignment. The are a simple list of bytes. Bytes and markers collectively
+//! are called slots. Furthermore, bytes can either be initialized or uninitialized.
+//! 
+//! Makers are stored in the [`Markers`](crate::slots::Marker) type, which is itself a slot.
+//! The types [`Init`](crate::slots::Init) and [`Uninit`](crate::slots::Uninit) represent
+//! initialized an uninitialized bytes of memory. Each is a single byte wide.
+//! 
+//! A [`SlotList`](crate::slots::SlotList) is a linked list where every element is a [`Slot`](crate::slots::Slot).
+
 use crate::hlist::*;
 use typenum::{*, operator_aliases::Sum};
 use core::ops::Add;
 
+/// The size of a [`SlotList`](crate::slots::SlotList)
 pub type Size<L> = <L as SlotList>::Size;
 
+/// Either an [`Init`](crate::slots::Init), [`Uninit`](crate::slots::Uninit), [`Marker`](crate::slots::Marker::Marker)
+/// 
+/// see [module](crate::slots) docs for details
 pub trait Slot {
     type Size: Unsigned;
 }
 
+/// A linked list of [`Slot`](crate::slots::Slot)s
 pub trait SlotList {
     type Size: Unsigned;
 }
 
+/// A property of the directly bytes that follow it
+/// 
+/// `bool` means the next byte is either a 0 or a 1
+/// 
+/// `&'a T` means that the next pointer width number of bytes
+/// represents a valid shared reference to `T` with lifetime `'a`
+/// 
+/// `&'a mut T` means that the next pointer width number of bytes
+/// represents a valid unique reference to `T` with lifetime `'a`
+/// 
+/// [`NonZero<N>`](crate::slots::NonZero) see type docs
 pub trait Marker {}
 
+/// A linked list of [`Marker`](crate::slots::Marker::Marker)s
 pub trait MarkerList {}
 
 impl SlotList for Nil {
@@ -28,8 +59,18 @@ where
     type Size = Sum<P::Size, T::Size>;
 }
 
+/// Initialized memory
 pub struct Init;
+
+/// Uninitialized memory
 pub struct Uninit;
+
+/// Stores a linked list of [`Marker`](crate::slots::Marker)s
+pub struct Markers<T>(T);
+
+/// A marker that states that the next `N` bytes are guaranteed to contain at
+/// least 1 bit that is set. (i.e. if the next `N` bytes are interpreted as
+/// an integer, that integer would be non-zero)
 pub struct NonZero<N>(N);
 
 impl Slot for Init {
@@ -39,8 +80,6 @@ impl Slot for Init {
 impl Slot for Uninit {
     type Size = U1;
 }
-
-pub struct Markers<T>(T);
 
 impl<P: MarkerList, T: Marker> Slot for Markers<Cons<P, T>> {
     type Size = U0;
